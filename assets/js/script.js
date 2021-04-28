6df0f7d0ca16b6ca2d645d5fa4516c8787eeaad7
Sharni Zaugg <sharni.greener@gmail.com>
2021-04-27 18:52:37 -0600

    basic search bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 
 //nutrition api set up
-
 function nutritionApi(food){
     var apiUrl = "https://api.edamam.com/api/food-database/v2/parser?app_id=f6bcb10b&app_key=f433e474ba7ce9d3e2329315b7ff8586&ingr=" + food;
     
@@ -10,6 +9,9 @@ function nutritionApi(food){
         }).then(function(data){
             console.log(data);
             console.log(data.hints[0].food.nutrients.ENERC_KCAL);
+            console.log(data.hints[0].food.nutrients.PROCNT);
+            console.log(data.hints[0].food.nutrients.CHOCDF);
+            console.log(data.hints[0].food.nutrients.FAT);
         })
     
 }
@@ -17,52 +19,27 @@ function nutritionApi(food){
 nutritionApi("pear");
 
 
-//chart api set up. Commented out while experimenting with chart.js
-// google.charts.load('current', {packages: ['corechart']});
-// google.charts.setOnLoadCallback(drawChart);
-
-// function drawChart(mon, tue, wed, thur, fri, sat, sun){
-//     var data = google.visualization.arrayToDataTable([
-//         ["Day", "Minutes of Exercise", {role: "style"}],
-//         ["Monday", 25, "red"],
-//         ["Tuesday", 40, "red"],
-//         ["Wednesday", 13, "red"],
-//         ["Thursday", 35, "red"],
-//         ["Friday", 30, "red"],
-//         ["Saturday", 35, "red"],
-//         ["Sunday", 10, "green"]
-//     ]);
-
-//     var options = {
-//         height: 200,
-//         width: 300
-//     };
-//     var chart = new google.visualization.ColumnChart(document.getElementById("fitnessChart"));
-//     chart.draw(data, options);
-// }
-
-//chart.js playground
+//chart.js chest tracking chart
 var daysOfWeek = JSON.parse(localStorage.getItem("days")) || {
-    Monday: 0,
-    Tuesday: 0,
-    Wednesday: 0,
-    Thursday: 0,
-    Friday: 0,
-    Saturday: 0,
-    Sunday: 0
+    Week1: 0,
+    Week2: 0,
+    Week3: 0,
+    Week4: 0,
+    Week5: 0,
+    Week6: 0
 };
 
 function displayChart(arr){
-    $("#dynamic-chart").empty().append(`<canvas id="fitnessChart" width="400" height="400"></canvas>
+    $("#dynamic-chart-chest").empty().append(`<canvas id="fitnessChart" width="400" height="400"></canvas>
     `)
     //var fitnessChart = document.getElementById("fitnessChart").getContext("2d");
 
     var barChart = new Chart(fitnessChart, {
-        type: "bar",
+        type: "polarArea",
         data:{
-            labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+            labels: ["Week 1", "Week 2", "Week 3", "Week 4", "Week 5", "Week 6"],
             datasets: [{
-                label: "Minutes of Exercise",
+                label: "Weight Lifted",
                 data: arr
             }]
         },
@@ -80,8 +57,8 @@ var newData = {
 }
 function getUserInput(event){
     event.preventDefault();
-    newData[[document.getElementById("date").value]] = parseInt(newData[[document.getElementById("date").value]]) + parseInt(document.getElementById("minutes").value);
-    localStorage.setItem("days", JSON.stringify(newData));
+    newData[[document.getElementById("date").value]] = parseInt(newData[[document.getElementById("date").value]]) + parseInt(document.getElementById("pounds").value);
+    localStorage.setItem("weeks", JSON.stringify(newData));
 
     displayChart(Object.values(newData));
 }
