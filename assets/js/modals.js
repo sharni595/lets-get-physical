87bd1b80724bc566cd47184a1dcd06465d42e3dd
Malcolm Easton <eastonmalcolm@gmail.com>
2021-04-28 21:47:27 -0600

    added fancy zoomy transitions + click to exit

--- assets/js/modals.js
+++ assets/js/modals.js
@@ -5,6 +5,10 @@ $('.modal-body') //Need to add a standard element for every modal's body at the 
             $(e.target).closest('.modal').removeClass('modal-active');
 });
 
+$('.modal-active').click((e)=>{ //If we click anywhere but the modal body, rmeove the modal
+    $(e.target).removeClass('modal-active');
+});
+
 function openModal(modalID){ //This function is designed to be assigned to buttons.  Just feed it the id of the modal and it'll open that modal
     $('.modal-active').removeClass('modal-active');
     $(`#${modalID}`).addClass('modal-active');
