da0928a142f424eaf3a426c990fcff90c2348db1
Malcolm Easton <eastonmalcolm@gmail.com>
2021-05-01 18:02:12 -0600

    removed more extraneous vars

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,3 @@
-
-var calories = document.getElementById("calories");
-var protein = document.getElementById("protein");
-var carbs = document.getElementById("carbs");
-var fat = document.getElementById("fat");
-
 //chart.js chest tracking chart
 var workoutData = JSON.parse(localStorage.getItem("workoutData")) || {
     chest: [0,0,0,0,0,0],
@@ -28,17 +22,14 @@ $('#nutrition-search').click(nutritionApi);
 function nutritionApi(){
     if(!$('#nutrition-input').val()) return;
     var apiUrl = "https://api.edamam.com/api/food-database/v2/parser?app_id=f6bcb10b&app_key=f433e474ba7ce9d3e2329315b7ff8586&ingr=" + $('#nutrition-input').val();
-    
     fetch(apiUrl, {method: "GET"})
         .then(response => response.json())
         .then(data => {
-            calories.textContent = data.hints[0].food.nutrients.ENERC_KCAL.toFixed(0);
-            protein.textContent = data.hints[0].food.nutrients.PROCNT.toFixed(2);
-            carbs.textContent =  data.hints[0].food.nutrients.CHOCDF.toFixed(2);
-            $('#fat').text(data.hints[0].food.nutrients.FAT.toFixed(2))
-            //fat.textContent = data.hints[0].food.nutrients.FAT.toFixed(2);
-        });
-    
+            $('#calories').textContent = data.hints[0].food.nutrients.ENERC_KCAL.toFixed(0);
+            $('#protein').textContent = data.hints[0].food.nutrients.PROCNT.toFixed(2);
+            $('#carbs').textContent =  data.hints[0].food.nutrients.CHOCDF.toFixed(2);
+            $('#fat').text(data.hints[0].food.nutrients.FAT.toFixed(2));
+    });
 }
 //This function deletes all chart canvases and then renders new ones with values from chartsObj
 function renderCharts(){
